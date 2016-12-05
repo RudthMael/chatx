7858f50a52802a2a2da728a946c1f5e7377bc0da
Rudth-Mael <rm@galite.net>
2016-12-05 18:12:48 +0100

    Persist token in local storage

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,18 +1,24 @@
 import { fromJS } from 'immutable';
 import { LOGIN, LOGIN_SUCCEEDED, LOGIN_FAILED } from '../actions';
 
+const LOCAL_STORAGE_TOKEN_KEY = 'chatx__token';
 const initialState = fromJS({
-  loading: false
+  loading: false,
+  token: localStorage.getItem(LOCAL_STORAGE_TOKEN_KEY)
 });
 
 export default (state = initialState, action) => {
   switch(action.type) {
   case LOGIN:
     return state.set('loading', true);
+
   case LOGIN_SUCCEEDED:
-    return state.set('loading', false);
+    localStorage.setItem(LOCAL_STORAGE_TOKEN_KEY, action.token)
+    return state.set('token', action.token);
+
   case LOGIN_FAILED:
     return state.set('loading', false);
+
   default:
     return state;
   }
