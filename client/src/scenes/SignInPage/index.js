ebf0e4ca2068235c97f2748221350dcee3bba4fd
Rudth-Mael <rm@galite.net>
2017-01-04 01:18:27 +0100

    Added link to register from login page

--- client/src/scenes/SignInPage/index.js
+++ client/src/scenes/SignInPage/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './index.css';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
 import { login } from '../../actions';
 import { getAuth } from '../../selectors';
 
@@ -31,38 +33,44 @@ class SignInPage extends React.Component {
 
   render() {
     return (
-      <div className="signin-page" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: 200 }}>
-        <form onSubmit={this.handleSubmit.bind(this)}>
-          <label className="pt-label">
-            Username
-            <input
-              className="pt-input"
-              style={{ width: 400 }}
-              type="text"
-              placeholder="Username"
-              dir="auto"
-              value={this.state.username}
-              onChange={this.handleLoginChange.bind(this)}
-              disabled={this.props.auth.get('loading')} />
-          </label>
+      <div className="signin-page">
+        <div className="signin-page__form">
+          <form onSubmit={this.handleSubmit.bind(this)}>
+            <label className="pt-label">
+              Username
+              <input
+                className="pt-input"
+                style={{ width: 400 }}
+                type="text"
+                placeholder="Username"
+                dir="auto"
+                value={this.state.username}
+                onChange={this.handleLoginChange.bind(this)}
+                disabled={this.props.auth.get('loading')} />
+            </label>
 
-          <label className="pt-label">
-            Password
-            <input
-              className="pt-input"
-              style={{ width: 400 }}
-              type="password"
-              placeholder="Password"
-              dir="auto"
-              value={this.state.password}
-              onChange={this.handlePasswordChange.bind(this)}
-              disabled={this.props.auth.get('loading')} />
-          </label>
+            <label className="pt-label">
+              Password
+              <input
+                className="pt-input"
+                style={{ width: 400 }}
+                type="password"
+                placeholder="Password"
+                dir="auto"
+                value={this.state.password}
+                onChange={this.handlePasswordChange.bind(this)}
+                disabled={this.props.auth.get('loading')} />
+            </label>
 
-          <div style={{ textAlign: 'center' }}>
-            <button type="Submit" className="pt-button" disabled={this.state.loading}>Log In</button>
-          </div>
-        </form>
+            <div style={{ textAlign: 'center' }}>
+              <button type="Submit" className="pt-button" disabled={this.state.loading}>Log In</button>
+            </div>
+          </form>
+        </div>
+
+        <div style={{ textAlign: 'center' }}>
+          No account? <Link to="/register">Sign up</Link>
+        </div>
       </div>
     )
   }
