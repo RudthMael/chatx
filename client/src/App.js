6771a289698a5fc685a1d6fcaebb2c1a746f64ff
Rudth-Mael <rm@galite.net>
2017-01-04 17:00:34 +0100

    Added home link

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux'
 import { Intent, Position, Toaster } from "@blueprintjs/core";
+import { Link } from 'react-router';
 
 class App extends Component {
   componentWillMount() {
@@ -24,6 +25,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <nav className="pt-navbar" style={{ marginBottom: 60 }}>
+          <div className="pt-navbar-group pt-align-left">
+            <Link to="/" className="pt-button pt-minimal pt-icon-home">
+              Home
+            </Link>
+          </div>
+        </nav>
+
         {this.props.children}
       </div>
     );
